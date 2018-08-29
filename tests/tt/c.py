f7e9e1396c6e0d6aa269ca176efdd57cd8f37471
mm <davismao@163.com>
2018-08-29 09:54:08 +0800

    测试

--- tests/tt/c.py
+++ tests/tt/c.py
@@ -6,10 +6,14 @@ def main():
                                socket.SOCK_STREAM, socket.SOL_TCP)
     af, socktype, proto, canonname, sa = addrs[0]
 
+
     s = socket.socket(af, socktype, proto)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     s.connect(sa)
+    #s.connect((host, port))
+    #s.connect(('192.168.7.165', port))
+
     print(s.recv(1024).decode())
 
     # s.close()
